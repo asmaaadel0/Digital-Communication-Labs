11cec74e5eaf2596bf437eaa89a9b52526d15345
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-05-01 01:52:57 +0300

    update lab 2

--- Lab 2/code.py
+++ Lab 2/code.py
@@ -1,11 +1,6 @@
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
-numOfBits = 10
-numOfSamplesPerBit = 10
-
 
 ############################################################
 ################## Convolution Function ####################
@@ -32,31 +27,21 @@ def applyConvolution(noisySamples, receivedFilterValues):
 ############################################################
 def calculateBERSimulated(bits, recievedBits):
     # applying lamda thresholed (lamda optimum = 0)
-    # receivedSamples = np.ones(numOfBits)
-    # receivedSamples += (-2 * (recievedBits < 0))
-    receivedSamples = (np.real(recievedBits) < 0).astype(int)
+    receivedSamples = np.ones(numOfBits)
+    # later why * -2
+    receivedSamples += (-2 * (recievedBits < 0))
+    # receivedSamples = (np.real(recievedBits) < 0).astype(int)
     # calculate probability of error
     error_probability = np.sum(receivedSamples != bits)
     error_probability /= numOfBits
-    # later important always error_probability = 1
+    # DONE later important always error_probability = 1
     return error_probability
-
-
-# def BER_sim(bits, recievedBits):
-#         decoded=np.zeros(numOfBits)
-#         k=0
-#         for j in range(numOfSamplesPerBit-1,len(recievedBits),numOfSamplesPerBit):
-#             if recievedBits[j]>0 and k<numOfBits:
-#                 decoded[k]=1
-#             k+=1
-#         #------------------------------
-#         return np.sum(decoded != bits)/numOfBits
 
 
 ############################################################
 ######################## main function #####################
 ############################################################
-def func(receivedFilterMatched, ramp):
+def func(receivedFilterMatched, type, plot):
 
     experimentalBER = []
     theorticalBER = []
@@ -68,7 +53,7 @@ def func(receivedFilterMatched, ramp):
         # generating random noise
         generatedNoise = np.random.normal(
             # later waht is this??
-            0, E/(2*EOverN0_dec), size=numOfBits*numOfSamplesPerBit).reshape((numOfBits, numOfSamplesPerBit))
+            0, E/(2*EOverN0_dec), numOfBits*numOfSamplesPerBit).reshape((numOfBits, numOfSamplesPerBit))
 
         # add noise to samples
         noisySamples = samples + generatedNoise
@@ -77,28 +62,40 @@ def func(receivedFilterMatched, ramp):
         filteredSamples, recievedBits = applyConvolution(
             noisySamples, receivedFilterMatched)
         # append BER simulated for plotting
-        experimentalBER.append(calculateBERSimulated(numOfBits, recievedBits))
+        experimentalBER.append(calculateBERSimulated(bits, recievedBits))
         # append BER theortical for plotting
 
-        if ramp == 1:
-            theorticalBER.append(math.erfc((3**0.5/2) * EOverN0_dec ** 0.5))
+        if type == 3:
+            theorticalBER.append(0.5*math.erfc((3**0.5/2) * EOverN0_dec ** 0.5))
         #later math.erfc(EOverN0_dec ** 0.5) or 0.5*math.erfc(EOverN0_dec ** 0.5) ??
         else: 
           theorticalBER.append(math.erfc(EOverN0_dec ** 0.5))
+    if plot == 1:
 
-    # ploting
-    plt.figure()
-    plt.plot(range(0, filteredSamples.flatten(
-    ).shape[0]), filteredSamples.flatten(), label="bit value")
+        # ploting
+        plt.figure()
+        plt.plot(range(0, filteredSamples.flatten(
+        ).shape[0]), filteredSamples.flatten(), label="bit value")
 
-    plt.xlabel('time')
-    plt.ylabel('bit')
-    plt.title('output with Delta Filter')
+        plt.xlabel('time')
+        plt.ylabel('bit')
+        if type == 1:
+            plt.title('output with matched Filter')
 
-    plt.grid()
-    plt.show()
+        elif type == 2:
+            plt.title('output with no Filter')    
+
+        else:
+            plt.title('output with ramp Filter')        
+
+        plt.grid()
+        plt.show()
     return experimentalBER, theorticalBER
 
+
+#constants
+numOfBits = 10
+numOfSamplesPerBit = 10
 
 ############################################################
 ############### Generating bits and samples ################
@@ -116,36 +113,68 @@ bits = np.random.choice([-1, 1], size=(numOfBits,), p=[1./2, 1./2])
 samples = (np.asarray([[bits[i] for i in range(numOfBits)]
            for _ in range(numOfSamplesPerBit)])).T
 
+
 # receive with matched filter
 filter_1 = np.ones(numOfSamplesPerBit)
-experimentalBER_1, theorticalBER_1 = func(filter_1, 0)
+experimentalBER_1, theorticalBER_1 = func(filter_1, 1, 1)
 
 # receive with no filter
 filter_2 = None
-experimentalBER_2, theorticalBER_2 = func(filter_2, 0)
+experimentalBER_2, theorticalBER_2 = func(filter_2, 2, 1)
 
 # receive with ramp filter
-# filter_2 = np.random.uniform(low=0, high=3**0.5, size=numOfSamplesPerBit)
+# filter_3 = np.random.uniform(low=0, high=3**0.5, size=numOfSamplesPerBit)
 # later improve 
 # later we have to review the filter and the result
 filter_3 = np.linspace(0, 10, numOfSamplesPerBit)
 for i in range(len(filter_3)):
     filter_3[i] = np.sqrt(3) * filter_3[i]
-experimentalBER_3, theorticalBER_3 = func(filter_3, 1)
+experimentalBER_3, theorticalBER_3 = func(filter_3, 3, 1)
+
+
+# constants
+numOfBits = 100000
+numOfSamplesPerBit = 10
+
+############################################################
+############### Generating bits and samples ################
+############################################################
+
+# generate random bits with equal probability
+bits = np.random.choice([-1, 1], size=(numOfBits,), p=[1./2, 1./2])
+# generate samples in range [numOfBits, numOfSamplesPerBit]
+samples = (np.asarray([[bits[i] for i in range(numOfBits)]
+           for _ in range(numOfSamplesPerBit)])).T
+
+
+# receive with matched filter
+filter_1 = np.ones(numOfSamplesPerBit)
+experimentalBER_1, theorticalBER_1 = func(filter_1, 1, 0)
+
+# receive with no filter
+filter_2 = None
+experimentalBER_2, theorticalBER_2 = func(filter_2, 2, 0)
+
+# receive with ramp filter
+filter_3 = np.linspace(0, 10, numOfSamplesPerBit)
+for i in range(len(filter_3)):
+    filter_3[i] = np.sqrt(3) * filter_3[i]
+experimentalBER_3, theorticalBER_3 = func(filter_3, 3, 0)
 
 
 #ploting
 plt.figure()
-plt.plot(range(-10, 21), experimentalBER_1, label = "first BER experimental")
-plt.plot(range(-10, 21), experimentalBER_1, "--", label = "first BER theortical")
+# DONE later no plotting for theorticalBER_1
+plt.plot(range(-10, 21), experimentalBER_1, label = "First BER experimental")
+plt.plot(range(-10, 21), theorticalBER_1, "--", label = "First BER theortical")
 
-plt.plot(range(-10, 21), experimentalBER_2, label = "second experimental BER")
-plt.plot(range(-10, 21), experimentalBER_2, "--", label = "second theortical BER")
+plt.plot(range(-10, 21), experimentalBER_2, label = "Second experimental BER")
+plt.plot(range(-10, 21), theorticalBER_2, "--", label = "Second theortical BER")
 
-plt.plot(range(-10, 21), experimentalBER_3, label = "third experimental BER")
-plt.plot(range(-10, 21), experimentalBER_3, "--", label = "third theortical BER")
+plt.plot(range(-10, 21), experimentalBER_3, label = "Third experimental BER")
+plt.plot(range(-10, 21), theorticalBER_3, "--", label = "Third theortical BER")
 
-plt.xlabel('E_Over_N0(DB)')
+plt.xlabel('E/N0(DB)')
 plt.ylabel('Bit Error Rate')
 plt.yscale('log')
 plt.ylim(10**(-5))
